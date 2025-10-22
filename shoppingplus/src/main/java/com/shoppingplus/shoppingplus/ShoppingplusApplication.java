5f48f15cacb2fd33fcf0f277e1b8638f765ced75
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-10-22 14:28:06 +0900

    Initialize Spring Boot project structure

---
+++ shoppingplus/src/main/java/com/shoppingplus/shoppingplus/ShoppingplusApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingplus.shoppingplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingplusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingplusApplication.class, args);
+	}
+
+}
---
+++ shoppingplus/src/test/java/com/shoppingplus/shoppingplus/ShoppingplusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingplus.shoppingplus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingplusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
