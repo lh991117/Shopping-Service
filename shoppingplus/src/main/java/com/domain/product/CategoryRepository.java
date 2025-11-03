a2efcbc5dd23b9499f741f57b332faffbc5a9efe
lh991117 <92137419+lh991117@users.noreply.github.com>
2025-11-03 15:10:23 +0900

    feat: repository

---
+++ shoppingplus/src/main/java/com/domain/product/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.domain.product;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
+    Optional<Category> findByName(String name);
+}
